d3ef1bb096f54363c29ed81e95be2613d299ab66
Blake Kostner <btkostner@gmail.com>
2016-01-05 20:23:44 -0700

    no touch backs

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -28,3 +28,18 @@ function get_path (path) {
 
   return path.substring(pos, -1) + '/'
 }
+
+function toggle_box (self, id) {
+  var element = document.getElementById(id)
+  if (element == null) {
+    return
+  }
+
+  if (element.style.display === 'block') {
+    element.style.display = 'none'
+    self.src = get_path(self.src) + 'coll_open.png'
+  } else {
+    element.style.display = 'block'
+    self.src = get_path(self.src) + 'coll_close.png'
+  }
+}

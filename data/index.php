72079b2f936f0e57f267b6d5cdf7229c61b0e61f
Daniel Foré <daniel@elementary.io>
2016-01-11 13:45:13 -0800

    address keyboard navigation

--- data/index.php
+++ data/index.php
@@ -64,7 +64,7 @@ function get_title () {
   <body>
     <nav>
       <div id="search-box">
-        <input id="search-field" type="text" placeholder="Search" autofocus="autofocus" autocompletion="off" autosave="search" /><img id="search-field-clear" src="/images/clean.svg" />
+        <input id="search-field" type="text" placeholder="Search" autocompletion="off" autosave="search" /><img id="search-field-clear" src="/images/clean.svg" />
       </div>
       <a class="title" href="/index.htm"><img alt="Valadoc" src="images/logo.svg"/></a>
       <span class="subtitle">Stays crunchy, even in milk.</span>
